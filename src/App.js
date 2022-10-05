2e86604d68e631697ee9761de868e775f10bc187
nicetryharshit <harshitdubeyhd@gmail.com>
2022-10-05 18:37:23 +0530

    correct page name

--- src/App.js
+++ src/App.js
@@ -1,33 +1,66 @@
 import { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+export default function App()
+{
   const [bDayTotal, setBDayTotal] = useState(0);
   const [luckyNumber, setLuckyNumber] = useState(0);
   const [error, setError] = useState("");
   const [result, setResult] = useState("");
+  const [resultOption, setResultOption] = useState(-1);
 
-  const onBirthdayChange = (event) => {
+  const onBirthdayChange = (event) =>
+  {
     let total = 0;
     const date = event.target.value.replaceAll("-", "");
-    for (let digit of date) {
+    for (let digit of date)
+    {
       total = total + Number(digit);
     }
 
     setBDayTotal(total);
   };
-  const onLuckyNumberChange = (event) => {
+  const onLuckyNumberChange = (event) =>
+  {
     event.target.value = Math.round(event.target.value);
     setLuckyNumber(event.target.value);
   };
-  const onCheck = () => {
+
+  const setResultStyle = () =>
+  {
+    if (isNaN(bDayTotal) || isNaN(luckyNumber) || bDayTotal === 0)
+    {
+      return {
+        backgroundColor: "#CAE1ED"
+      }
+    }
+    else
+    {
+      if (Number(bDayTotal) % Number(luckyNumber) === 0) 
+      {
+        return {
+          backgroundColor: "#98FCA4"
+        }
+      }
+      else 
+      {
+        return {
+          backgroundColor: "#FCA998"
+        }
+      }
+    }
+  }
+  const onCheck = () =>
+  {
     setError("");
     setResult("");
-    if (isNaN(bDayTotal) || isNaN(luckyNumber) || bDayTotal === 0) {
+    if (isNaN(bDayTotal) || isNaN(luckyNumber) || bDayTotal === 0)
+    {
       setError("Please fill correct numerical values in both fields");
-    } else {
-      if (Number(bDayTotal) % Number(luckyNumber) === 0) setResult("Lucky!");
-      else setResult("Unlucky!");
+    } else
+    {
+      if (Number(bDayTotal) % Number(luckyNumber) === 0) setResult("Yes, you're birthday is lucky!");
+      else setResult("Unlucky :( Try another lucky number?");
     }
   };
 
@@ -35,6 +68,7 @@ export default function App() {
     <div className="App">
       <section>
         <h1> Is your birthday lucky?</h1>
+        <h3> Enter your birth date and a number in the fields below:</h3>
       </section>
       <section className="calc-body">
         <label>your date of birth: </label>
@@ -43,7 +77,7 @@ export default function App() {
         <input type="number" onChange={onLuckyNumberChange}></input>
         <button onClick={() => onCheck()}>Check</button>
       </section>
-      <section className="result">
+      <section className="result" style={setResultStyle()}>
         {error.length > 0 && <p>{error}</p>}
         {result.length > 0 && <p>{result}</p>}
       </section>
